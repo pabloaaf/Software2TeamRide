ced381ad278def54731d038da0a7a5493f87fdcf
palvaf02 <palvaf02@estudiantes.unileon.es>
2018-05-26 16:59:23 +0200

    new routes not working

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,18 +1,23 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import middleware from './server/middleware';
 import controller from './server/controller';
-import manager from './server/manager';
+import bbdd from './server/bbdd';
 
 class App {
 	public express: express.Aplication;
 
 	constructor () {
 		this.express = express();
-		manager;
+		bbdd;
 		this.express.use(bodyParser.urlencoded({ extended:false }));
 		this.express.use(bodyParser.json());
-		this.express.use('/v0', middleware, controller.contRoutes());
+		this.express.use('/v0', controller.contRoutes());
+		this.express.use('', this.pruebas);
+	}
+
+	private pruebas(req, res, next) {
+		console.log(req.body);
+		next();
 	}
 }
 export default new App().express;
--- Back-end/src/server/controller.ts
+++ Back-end/src/server/controller.ts
@@ -1,15 +1,20 @@
 import * as express from 'express'
-import manager from './manager';
+import bbdd from './bbdd';
 
 class BasicsController {
 
     constructor () {
     }
 
-    // rutas Controller
+    // rutas Controller **********************************
     public contRoutes() {
         let router = express.Router();
-        // responde a los get de la misma ruta
+        //registro
+        router.route('/login/')
+            .get(this.login)
+            .post(this.register);
+
+        // teams
         router.route('/teams/')
             .get(this.devolverTeams)
             .post(this.actualizarTeams);
@@ -18,20 +23,89 @@ class BasicsController {
             .get(this.devolverTeamId)
             .put(this.actualizarTeamId)
             .delete(this.borrarTeamId);
+        /*
+        /v0/teams  GET     Get all the teams.
+        /v0/teams  POST    Create a teams.
+        /v0/teams/:team_id     GET     Get a single team.
+        /v0/teams/:team_id     PUT     Update a team with new info.
+        /v0/teams/:team_id     DELETE  Delete a team. 
+        */
+
+        //cars
+        router.route('/cars/')
+            .get(this.devolverCars)
+            .post(this.actualizarCars);
+
+        router.route('/cars/:cars_id')
+            .get(this.devolverTeamId)
+            .put(this.actualizarCarId)
+            .delete(this.borrarCarId);
+        
+        //players
+        router.route('/players/')
+            .get(this.devolverPlayers)
+            .post(this.actualizarPlayers);
+
+        router.route('/players/:players_id')
+            .get(this.devolverPlayerId)
+            .put(this.actualizarPlayerId)
+            .delete(this.borrarPlayerId);
+
+        //pavilions
+        router.route('/pavilions/')
+            .get(this.devolverPavilions)
+            .post(this.actualizarPavilions);
+
+        router.route('/pavilions/:pavilions_id')
+            .get(this.devolverPavilionId)
+            .put(this.actualizarPavilionId)
+            .delete(this.borrarPavilionId);
 
         return router;
     }
 
-    // funciones Controller
-    private function1 (req, res, next) {
-        res.json({ mensaje: 'esta en el controller.'});
-        next();
+    // funciones Controller **********************************
+    private login (req, res, next) {
+        console.log('respuesta devolverTeamId');
+        bbdd.login()
+        .then(
+            value => {
+                res.send('Aqui se devolvera un token de loggeo');
+                //bucle que busca si el usuario existe
+                //ToDo
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send('Usuario no encontrado');
+                res.status(404).end();
+            }
+        );
     }
 
+    private register (req, res, next) {
+        console.log('respuesta devolverTeamId');
+        bbdd.register()
+        .then(
+            value => {
+                res.json('Usuario registrado');
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    //+++++++++++++++++++  TEAM  ++++++++++++++++++
 
     private devolverTeams (req, res, next) {
         console.log('respuesta devolverTeams');
-        manager.leerEquipos()
+        bbdd.infoTeams()
         .then(
             value => {
                 res.json(value);
@@ -41,13 +115,14 @@ class BasicsController {
             err => {
                 console.log('err');
                 res.send(err);
+                res.status(404).end();
             }
         );
     }
 
     private actualizarTeams (req, res, next) {
         console.log('respuesta actualizarTeams');
-        manager.añadirEquipo(req.body.name)
+        bbdd.addTeam(req.body.name)
         .then(
             value => {
                 res.json(value);
@@ -57,35 +132,338 @@ class BasicsController {
             err => {
                 console.log('err');
                 res.send(err);
+                res.status(404).end();
             }
         );
     }
 
     private devolverTeamId (req, res, next) {
         console.log('respuesta devolverTeamId');
-        next();
+        bbdd.infoTeam(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
     }
 
 
     private actualizarTeamId (req, res, next) {
         console.log('respuesta actualizarTeamId');
-        next();
+        bbdd.updateTeam(req.params.id, req.body.name) //pasar id y un nombre nuevo
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
     }
 
     private borrarTeamId (req, res, next) {
         console.log('respuesta borrarTeamId');
-        next();
+        bbdd.deleteTeam(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    //+++++++++++++++++++ CAR ++++++++++++++++++++++
+
+    private devolverCars (req, res, next) {
+        console.log('respuesta devolverTeams');
+        bbdd.infoCars()
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private actualizarCars (req, res, next) {
+        console.log('respuesta actualizarTeams');
+        bbdd.addCar(req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private devolverCarId (req, res, next) {
+        console.log('respuesta devolverTeamId');
+        bbdd.infoCar(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+
+    private actualizarCarId (req, res, next) {
+        console.log('respuesta actualizarTeamId');
+        bbdd.updateCar(req.params.id, req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private borrarCarId (req, res, next) {
+        console.log('respuesta borrarTeamId');
+        bbdd.deleteCar(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    //+++++++++++++++++++  PLAYERS  ++++++++++++++++++
+
+    private devolverPlayers (req, res, next) {
+        console.log('respuesta devolverTeams');
+        bbdd.infoPlayers()
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private actualizarPlayers (req, res, next) {
+        console.log('respuesta actualizarTeams');
+        bbdd.addPlayer(req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private devolverPlayerId (req, res, next) {
+        console.log('respuesta devolverTeamId');
+        bbdd.infoPlayer(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+
+    private actualizarPlayerId (req, res, next) {
+        console.log('respuesta actualizarTeamId');
+        bbdd.updatePlayer(req.params.id, req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    private borrarPlayerId (req, res, next) {
+        console.log('respuesta borrarTeamId');
+        bbdd.deletePlayer(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+    }
+
+    //+++++++++++++++  PAVILIONS  ++++++++++++++++++
+
+    private devolverPavilions (req, res, next) {
+        console.log('respuesta devolverTeams');
+        bbdd.infoPavilions()
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+
+        //añadir teams_pav_rel aqui
+    }
+
+    private actualizarPavilions (req, res, next) {
+        console.log('respuesta actualizarTeams');
+        bbdd.addPavilion(req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+
+        //añadir teams_pav_rel aqui
+
+    }
+
+    private devolverPavilionId (req, res, next) {
+        console.log('respuesta devolverTeamId');
+        bbdd.infoPavilion(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+
+        //añadir teams_pav_rel aqui
+    }
+
+
+    private actualizarPavilionId (req, res, next) {
+        console.log('respuesta actualizarTeamId');
+        bbdd.updatePavilion(req.params.id, req.body.playerId, req.body.teamId, req.body.gas, req.body.model)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+
+        //añadir teams_pav_rel aqui
+    }
+
+    private borrarPavilionId (req, res, next) {
+        console.log('respuesta borrarTeamId');
+        bbdd.deletePavilion(req.params.id)
+        .then(
+            value => {
+                res.json(value);
+                next();
+            }
+        ).catch(
+            err => {
+                console.log('err');
+                res.send(err);
+                res.status(404).end();
+            }
+        );
+
+        //añadir teams_pav_rel aqui
     }
 }
 
 export default new BasicsController();
 
-/*
-
-/api/teams  GET     Get all the teams.
-/api/teams  POST    Create a teams.
-/api/teams/:team_id     GET     Get a single team.
-/api/teams/:team_id     PUT     Update a team with new info.
-/api/teams/:team_id     DELETE  Delete a team. 
-
-*/
