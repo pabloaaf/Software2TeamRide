295dc352444e86cd24b097dfd55f670b28fb9aa9
palvaf02 <palvaf02@estudiantes.unileon.es>
2018-05-28 12:57:11 +0200

    allow cors to server

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -2,13 +2,27 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import controller from './server/controller';
 import bbdd from './server/bbdd';
+import * as cors from "cors";
+
+//options for cors midddleware
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
+  preflightContinue: false
+};
 
 class App {
-	public express: express.Aplication;
+	public express;
 
 	constructor () {
 		this.express = express();
 		bbdd;
+		//use cors middleware
+		this.express.use(cors(options));
+		//enable pre-flight
+		this.express.options("*", cors(options));
 		this.express.use(bodyParser.urlencoded({ extended:false }));
 		this.express.use(bodyParser.json());
 		this.express.use('/v0', controller.contRoutes());
