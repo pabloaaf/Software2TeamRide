00729f6b511b86439c12a5582b4b37188d8708d2
XerontD <36925116+XerontD@users.noreply.github.com>
2018-06-07 18:22:52 +0200

    Historico funcional

--- Front-end/src/pages/Details/View-Historic/info-travel/info-travel.ts
+++ Front-end/src/pages/Details/View-Historic/info-travel/info-travel.ts
@@ -17,38 +17,90 @@ import { HttpProvider } from '../../../../providers/http/http';
 })
 export class InfoTravelPage {
 	public date:string;
-	public tripPlayers:tripPlayers[];
-	public tripCars:tripCars[];
 	public players:players[];
 	public showPlayer:players[];
 	public cars: cars[];
 	public showCars:cars[];
-	public i:number;
-	public j:number;
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpProvider) {
   	this.date = navParams.data;
-  	this.tripCars = [];
-  	this.tripPlayers = [];
   	this.players = [];
   	this.cars = [];
   	this.showPlayer = [];
   	this.showCars = [];
   	let contador = 0;
+  	let contador2 = 0;
+  	let auxTripPlayers = [];
+  	let auxTripCars = [];
+  	let i =0;
+  	let j = 0;
   	this.http.infoTripCars(this.date).subscribe((cars:tripCars[])=>{
-	    this.tripCars = cars;
-
+  		for(i=0;i<cars.length;i++){
+	    	auxTripCars[i] = cars[i].carId;
+	    }
 	});
 	this.http.infoTripPlayers(this.date).subscribe((play:tripPlayers[])=>{
-	    this.tripPlayers = play;
-
+	    for(i=0;i<play.length;i++){
+	    	auxTripPlayers[i] = play[i].playerId;
+	    }
+	    
 
 	});
 	this.http.getPlayers().subscribe((play:players[])=>{
 	    this.players = play;
+
+	    for(i=0;i<auxTripPlayers.length;i++){
+			for(j=0;j<play.length;j++){
+				if(auxTripPlayers[i] == play[j].id){
+					this.showPlayer[contador] = play[j];
+					contador++;
+					break;
+				}
+			}
+		}
+
+	    
 	});
 	this.http.getCarList().subscribe((car:cars[])=>{
 	    this.cars = car;
+	    if(auxTripCars.length != 0){
+	    	for(i=0;i<auxTripCars.length;i++){
+				for(j=0;j<car.length;j++){
+					if(auxTripCars[i] == car[j].id){
+						this.showCars[contador2] = car[j];
+						contador2++;
+						break;
+					}
+				}
+			}
+	    }
+
 	});
+	
+	//console.log(this.tripPlayers + " " +this.players);
+	//console.log(this.tripPlayers.length + " " +this.players.length)
+	/*
+	if(auxTripCars.length != 0){
+	    	for(i=0;i<auxTripCars.length;i++){
+				for(j=0;j<car.length;j++){
+					if(auxTripCars[i] == car[j].id){
+						this.showCars[contador2] = car[j];
+						console.log(this.showCars[contador2]);
+						//console.log(contador + " " + this.showCars);
+						contador2++;
+						break;
+					}
+				}
+			}
+	    }
+
+
+
+
+
+
+
+
+
 	for(this.i=0;this.i<this.tripPlayers.length;this.i++){
 		for(this.j=0;this.j<this.players.length;this.j++){
 			console.log(tripPlayers[this.i].playerId+ " " + this.players[this.j].id);
@@ -57,8 +109,8 @@ export class InfoTravelPage {
 				break;
 			}
 		}
-	}
-	console.log(this.showPlayer);
+	}*/
+	
 	/*contador = 0;
 	for(let i=0;i<cars.length;i++){
 		for(let j=0;j<cars.length;j++){
